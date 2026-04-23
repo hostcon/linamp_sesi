f8e974df77fe3e369e93992c74c8eb67f36622a7
Wagner R. da Silva <wagaum@gmail.com>
2026-04-23 18:09:11 -0300

    Adiciona tela de login

--- linamp.py
+++ linamp.py
@@ -1,3 +1,14 @@
-print("Olá Mundo")
+import tkinter as tk
 
-# Aqui começa a alteração1
+janela = tk.Tk()
+janela.title("Player")
+janela.geometry("300x200")
+
+def play():
+    print("Play")
+
+    
+
+btn = tk.Button(janela, text="Play", command=play)
+btn.pack()
+janela.mainloop()
